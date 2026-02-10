2f4c12953b0efc251750c0a939e45a2ac747fe59
tanmayeepeethala <tanmayeepeethala09@gmail.com>
2026-02-10 22:46:19 +0530

    Descending order of elements in the list

---
+++ 40.py
@@ -0,0 +1,8 @@
+lst=list(map(int,input("Enter elements into the list:").split(",")))
+for i in range(len(lst)):
+	for j in range(len(lst)):
+		if lst[i]>lst[j]:
+			temp=lst[i]
+			lst[i]=lst[j]
+			lst[j]=temp
+print(lst)
